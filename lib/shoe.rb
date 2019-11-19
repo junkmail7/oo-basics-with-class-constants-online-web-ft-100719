15af57d1e9077e3af6c06c99514641ccb6854ea5
William Pannell <lpann5@gmail.com>
2019-11-19 23:52:56 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,17 @@
+<<<<<<< HEAD
 require 'pry'
+=======
+#require 'pry'
+>>>>>>> adc6c398f30eb0bcb73733c7a63ed01c1bd47460
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   BRANDS = []
-
+    nuarray=[]
   def initialize(brand)
     @brand = brand
+<<<<<<< HEAD
     nuarray=[]
     nuarray << brand
     x=0
@@ -15,6 +20,14 @@ class Shoe
     BRANDS << nuarray[x]
     end
     #binding.pry
+=======
+
+    nuarray << brand
+    nuarray=nuarray.uniq
+    x=0
+    while x!=nuarray.size
+    BRANDS << nuarray[x]
+>>>>>>> adc6c398f30eb0bcb73733c7a63ed01c1bd47460
     x=x+1
     end
   end
@@ -25,3 +38,7 @@ class Shoe
   end
 
 end
+<<<<<<< HEAD
+=======
+#binding.pry
+>>>>>>> adc6c398f30eb0bcb73733c7a63ed01c1bd47460
